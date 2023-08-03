e241ead30d7044702f9d07773d6d9f0ba44dd264
lin <944142140@qq.com>
2023-08-03 17:29:31 +0800

    上传项目所有代码

---
+++ include/server/model/friendmodel.hpp
@@ -0,0 +1,17 @@
+#ifndef FRIENDMODEL_H
+#define FRIENDMODEL_H
+
+#include "user.hpp"
+#include <vector>
+using namespace std;
+
+// 维护好友信息的操作接口方法
+class FriendModel {
+public:
+    // 添加好友关系
+    void insert(int userid, int friendid);
+ 
+    // 返回用户好友列表 friendid user表跟friend表的联合查询
+    vector<User> query(int userid);
+};
+#endif
